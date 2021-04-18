350a2bbea9fc23565f75a17fa693fb199c2b5ab2
미령 유 <kioku714@gmail.com>
2021-04-18 18:17:41 +0900

    feat: 차량 리스트 화면과 상세화면 구현

---
+++ lib/detail.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  DetailPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'W 모터스 라이칸 하이퍼스포트',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  '한화 약 38억 4천만 원',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        '라이칸 하이퍼스포트는 두바이에 거점하고 있는 W 모터스에서 탄생한 첫 번째 완성 차량입니다. '
+        '전 세계 단 7대밖에 존재하지 않는 슈퍼카인데요. '
+        '포르쉐 튜닝 스페셜리스트 RUF가 개발한 3.7리터 수평대향 6기통 트윈터보 엔진에서 최고출력 791마력, 최대토크 97.9kg.m을 발휘하고, 제로백은 2.8초를 기록합니다. '
+        '라이칸 하이퍼스포트는 두바이에 거점하고 있는 W 모터스에서 탄생한 첫 번째 완성 차량입니다. '
+        '전 세계 단 7대밖에 존재하지 않는 슈퍼카인데요. '
+        '포르쉐 튜닝 스페셜리스트 RUF가 개발한 3.7리터 수평대향 6기통 트윈터보 엔진에서 최고출력 791마력, 최대토크 97.9kg.m을 발휘하고, 제로백은 2.8초를 기록합니다. '
+        '라이칸 하이퍼스포트는 두바이에 거점하고 있는 W 모터스에서 탄생한 첫 번째 완성 차량입니다. '
+        '전 세계 단 7대밖에 존재하지 않는 슈퍼카인데요. '
+        '포르쉐 튜닝 스페셜리스트 RUF가 개발한 3.7리터 수평대향 6기통 트윈터보 엔진에서 최고출력 791마력, 최대토크 97.9kg.m을 발휘하고, 제로백은 2.8초를 기록합니다. '
+        '라이칸 하이퍼스포트는 두바이에 거점하고 있는 W 모터스에서 탄생한 첫 번째 완성 차량입니다. '
+        '전 세계 단 7대밖에 존재하지 않는 슈퍼카인데요. '
+        '포르쉐 튜닝 스페셜리스트 RUF가 개발한 3.7리터 수평대향 6기통 트윈터보 엔진에서 최고출력 791마력, 최대토크 97.9kg.m을 발휘하고, 제로백은 2.8초를 기록합니다. ',
+        softWrap: true,
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: [
+          Image.network(
+            'https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2FKCCAUTO%2F30f49582ea6d4a598561c1986efcf2ec.JPG',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          titleSection,
+          buttonSection,
+          textSection,
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:handle/detail.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,36 +13,140 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Auto Hub'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(title: 'Home'),
+        '/detail': (context) => DetailPage(title: '상세화면'),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class Person {
+  String name;
+  String profileImg;
+  String price;
+
+  Person({this.name, this.profileImg, this.price});
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Person> persons = [
+    Person(
+        name: 'W 모터스 라이칸 하이퍼스포트',
+        profileImg:
+            'https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2FKCCAUTO%2F30f49582ea6d4a598561c1986efcf2ec.JPG',
+        price: '약 38억 4천만원'),
+    Person(
+        name: '카니발 4세대9인승 시그니처',
+        profileImg:
+            'https://ci.encar.com/carpicture/carpicture06/pic2936/29364464_001.jpg?impolicy=heightRate&rh=480&cw=640&ch=480&cg=Center&wtmk=https://ci.encar.com/wt_mark/w_mark_03.png&t=20210323170454',
+        price: "3,932만원"),
+    Person(
+        name: '4시리즈 (F32)428i M 스포츠 컨버터블',
+        profileImg:
+            'https://ci.encar.com/carpicture/carpicture03/pic2933/29331711_001.jpg?impolicy=heightRate&rh=480&cw=640&ch=480&cg=Center&wtmk=https://ci.encar.com/wt_mark/w_mark_03.png&t=20210322112331',
+        price: '3,860만원'),
+    Person(
+        name: 'W 모터스 라이칸 하이퍼스포트',
+        profileImg:
+            'https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2FKCCAUTO%2F30f49582ea6d4a598561c1986efcf2ec.JPG',
+        price: '약 38억 4천만원'),
+    Person(
+        name: '카니발 4세대9인승 시그니처',
+        profileImg:
+            'https://ci.encar.com/carpicture/carpicture06/pic2936/29364464_001.jpg?impolicy=heightRate&rh=480&cw=640&ch=480&cg=Center&wtmk=https://ci.encar.com/wt_mark/w_mark_03.png&t=20210323170454',
+        price: "3,932만원"),
+    Person(
+        name: '4시리즈 (F32)428i M 스포츠 컨버터블',
+        profileImg:
+            'https://ci.encar.com/carpicture/carpicture03/pic2933/29331711_001.jpg?impolicy=heightRate&rh=480&cw=640&ch=480&cg=Center&wtmk=https://ci.encar.com/wt_mark/w_mark_03.png&t=20210322112331',
+        price: '3,860만원'),
+    Person(
+        name: 'W 모터스 라이칸 하이퍼스포트',
+        profileImg:
+            'https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2FKCCAUTO%2F30f49582ea6d4a598561c1986efcf2ec.JPG',
+        price: '약 38억 4천만원'),
+    Person(
+        name: '카니발 4세대9인승 시그니처',
+        profileImg:
+            'https://ci.encar.com/carpicture/carpicture06/pic2936/29364464_001.jpg?impolicy=heightRate&rh=480&cw=640&ch=480&cg=Center&wtmk=https://ci.encar.com/wt_mark/w_mark_03.png&t=20210323170454',
+        price: "3,932만원"),
+    Person(
+        name: '4시리즈 (F32)428i M 스포츠 컨버터블',
+        profileImg:
+            'https://ci.encar.com/carpicture/carpicture03/pic2933/29331711_001.jpg?impolicy=heightRate&rh=480&cw=640&ch=480&cg=Center&wtmk=https://ci.encar.com/wt_mark/w_mark_03.png&t=20210322112331',
+        price: '3,860만원'),
+  ];
+
+  Widget personDetailCard(Person) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Card(
+          child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DetailPage(title: '상세화면')),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(Person.profileImg)))),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    Person.name,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    Person.price,
+                    style: TextStyle(fontSize: 12),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+                children: persons.map((p) {
+              return personDetailCard(p);
+            }).toList()),
+          )),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
